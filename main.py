53cd30bf7c3e22c796816fe2e83e1ab7c105b548
alpergh <141397256+alpergh@users.noreply.github.com>
2023-09-09 20:53:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-isim=input(isim yaz)
-print(Hİ,isim)
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def mem(ctx):
+
+    sayi=random.choice(["0","1","2","3","4","5","6","7","8","9"])
+
+    if sayi in "123456":
+        with open('images/mem1.png','rb') as f:
+            picture = discord.File(f)
+
+    elif sayi in "789":
+        with open('images/mem2.jpg','rb') as f:
+            picture = discord.File(f)
+
+    elif sayi == "0" :
+        with open('images/mem3.jpg','rb') as f:
+            picture = discord.File(f)
+
+bot.run("")
